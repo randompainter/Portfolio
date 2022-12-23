e14232df4ae6e0d80fa2af0e92a02983e9d083a9
Pixel <83719773+randompainter@users.noreply.github.com>
2022-12-23 11:15:37 -0700

    Add files via upload

---
+++ Rock Paper Scissor/main.py
@@ -0,0 +1,47 @@
+#Import random module 
+import random 
+ 
+#Create a list of option options 
+option = ["Rock", "Paper", "Scissors"]
+
+#Assign the computer a random option
+computer_action = option[random.randint(0,2)] 
+ 
+# Variables
+player = False
+
+
+while player == False: 
+#Set the player to True 
+    print("|Rock, Paper and Scissors|")
+    player = input("\n(Rock), (Paper) or (Scissors)?: ").capitalize()
+    if player == computer_action: 
+        print("Computer chooses", computer_action)
+        print("Tie!") 
+    elif player == "Rock": 
+        if computer_action == "Paper": 
+            print("Computer chooses", computer_action)
+            print("You lose!") 
+        else: 
+            print("Computer chooses", computer_action)
+            print("You win!") 
+    elif player == "Paper": 
+        if computer_action == "Scissors": 
+            print("Computer chooses", computer_action)
+            print("You lose!") 
+        else: 
+            print("Computer chooses", computer_action)
+            print("You win!") 
+    elif player == "Scissors": 
+        if computer_action == "Rock": 
+            print("Computer chooses", computer_action)
+            print("You lose") 
+        else: 
+            print("Computer chooses", computer_action)
+            print("You win!") 
+    else: 
+        print("That's not a valid option, please check again") 
+    # Set player to False to continue loop
+    player = False 
+    # Assign computer with a new action
+    computer_action = option[random.randint(0,2)]  
